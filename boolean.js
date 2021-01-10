c26cc1e1e45aea7735f88791e5608657cca8d7b3
Gonzalo Martín <gonxal0infame@gmail.com>
2021-01-10 23:56:45 +0100

    Boolean

---
+++ boolean.js
@@ -0,0 +1,7 @@
+//Inicialización booleanos
+var soy_alto = true;
+var soy_fan = false;
+console.log(soy_alto, "soy alto");
+console.log(soy_fan, "soy fan");
+console.log(soy_alto && soy_fan);
+console.log(soy_alto || soy_fan);
---
+++ boolean.ts
@@ -0,0 +1,11 @@
+//Inicialización booleanos
+let soy_alto: boolean = true;
+let soy_fan: boolean = false;
+
+console.log(soy_alto, "soy alto");
+console.log(soy_fan, "soy fan");
+
+//Operador conjunción
+console.log(soy_alto && soy_fan);
+//Operador disyunción
+console.log(soy_alto || soy_fan);
