3543e0046524aef7b8fb13e79c8db2a32a45469c
Gonzalo Martín <gonxal0infame@gmail.com>
2021-01-10 23:48:53 +0100

    Variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+//Declaración de variable
+var nombre_variable;
+//Inicializar
+nombre_variable = true;
+//Declaración + Inicialización
+var equipo = "Real Madrid";
+//Constantes
+var pi = 3.14;
---
+++ variables.ts
@@ -0,0 +1,11 @@
+//Declaración de variable
+let nombre_variable;
+
+//Inicializar
+nombre_variable = true;
+
+//Declaración + Inicialización
+let equipo = "Real Madrid";
+
+//Constantes
+const pi = 3.14;
