c2f558e3e0af8bcb439b5ff0a0c5d73e2384aa5b
Gonzalo Martín <gonxal0infame@gmail.com>
2021-01-11 00:04:12 +0100

    Numeros

---
+++ number.js
@@ -0,0 +1,11 @@
+//Declaracion numeros
+var mi_edad = 23;
+var mi_nota = 16.55;
+var mi_nota2 = 18.55;
+//Error de ejecución: mi_edad = "hola";
+//Operadores aritméticos
+console.log(mi_nota + mi_nota2);
+console.log(mi_nota - mi_nota2);
+console.log(mi_nota * mi_nota2);
+console.log(mi_nota / mi_nota2);
+console.log(6 % 3);
---
+++ number.ts
@@ -0,0 +1,13 @@
+//Declaracion numeros
+let mi_edad: number = 23;
+let mi_nota: number = 16.55;
+let mi_nota2: number = 18.55;
+
+//Error de ejecución: mi_edad = "hola";
+
+//Operadores aritméticos
+console.log(mi_nota + mi_nota2);
+console.log(mi_nota - mi_nota2);
+console.log(mi_nota * mi_nota2);
+console.log(mi_nota / mi_nota2);
+console.log(6 % 3);
