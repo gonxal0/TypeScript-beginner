65edf17b31503fab9182b08d13e826a5ed5588cf
Gonzalo Martín <gonxal0infame@gmail.com>
2021-01-11 00:27:07 +0100

    Tuplas

---
+++ tupla.js
@@ -0,0 +1,11 @@
+var vector;
+var vector2;
+vector = [2, 5];
+vector2 = [5, 10];
+//Suma vectores
+var vector_resultante = [vector[0] + vector2[0], vector[1] + vector2[1]];
+console.log(vector_resultante);
+var tupla_5_elementos = [1, 2, 3, 4, 50];
+var profesor = ["Cesar", 1.80];
+var estudiante = ["Gonzalo", 1.83];
+console.log(estudiante[0] + " mide " + estudiante[1] + " metros");
---
+++ tupla.ts
@@ -0,0 +1,16 @@
+let vector: [number, number];
+let vector2: [number, number];
+
+vector = [2,5];
+vector2 = [5,10];
+
+//Suma vectores
+let vector_resultante: [number, number] = [vector[0] + vector2[0], vector[1] + vector2[1]];
+console.log(vector_resultante);
+
+let tupla_5_elementos: [number, number, number, number, number] = [1,2,3,4,50];
+
+let profesor: [string, number] = ["Cesar", 1.80];
+let estudiante: [string, number] = ["Gonzalo", 1.83];
+
+console.log(`${estudiante[0]} mide ${estudiante[1]} metros`);
